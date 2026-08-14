cee5ddffc19baa42317cf6e8236061764855d71f
Anka <runner@sjc22-be113-e959aadf-22bc-4a05-830b-4961f742c372-BA57BC925251.local>
2026-08-14 09:08:04 +0000

    Update axon-llm-dispenser cask v0.3.0

--- Casks/axon-llm-dispenser.rb
+++ Casks/axon-llm-dispenser.rb
@@ -1,6 +1,6 @@
 cask "axon-llm-dispenser" do
-  version "0.2.0"
-  sha256 "e69acc9f828f00c0cd20b7248f1c6e84c2181d620cf6e5a89d82fec61ce9b7f2"
+  version "0.3.0"
+  sha256 "9ee91bb3ed1bd1650141677672b66933c02607b5f2c75a645851a4abb1950102"
 
   url "https://github.com/dncore/axon-llm-dispenser/releases/download/v#{version}/axon-llm-dispenser-macos-v#{version}.zip"
   name "axon-llm-dispenser"
